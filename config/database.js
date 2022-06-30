534d20bfafd605afa3f24b061cb6d2a39107f433
ravielze <ravielz20@gmail.com>
2022-06-30 18:12:43 +0700

    Fix SSL error

--- config/database.js
+++ config/database.js
@@ -1,15 +1,23 @@
-module.exports = ({ env }) => ({
-    connection: {
-        client: "postgres",
+module.exports = ({ env }) => {
+    const config = {
         connection: {
-            host: env("DATABASE_HOST", "127.0.0.1"),
-            port: env.int("DATABASE_PORT", 5432),
-            database: env("DATABASE_NAME", "KAT"),
-            user: env("DATABASE_USERNAME", "postgres"),
-            password: env("DATABASE_PASSWORD", "password"),
-            ssl: {
-                rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+            client: "postgres",
+            connection: {
+                host: env("DATABASE_HOST", "127.0.0.1"),
+                port: env.int("DATABASE_PORT", 5432),
+                database: env("DATABASE_NAME", "KAT"),
+                user: env("DATABASE_USERNAME", "postgres"),
+                password: env("DATABASE_PASSWORD", "password"),
+                ssl: false,
             },
         },
-    },
-});
+    };
+    const production = env.bool("PRODUCTION", false);
+    if (production) {
+        config.ssl = {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        };
+    }
+
+    return config;
+};
