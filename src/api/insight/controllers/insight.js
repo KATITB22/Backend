630c2f973ad16a175d7cba800dda344fff942f63
Fabian <65700687+fabiansdp@users.noreply.github.com>
2022-08-20 10:52:38 +0700

    Story/form insight (#9)
    * create insight form api
    
    * edit create insight route
    
    * add get insight
    
    * disable publish system
    
    * handle null insight data
    
    * balikin debug state

---
+++ src/api/insight/controllers/insight.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ *  insight controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::insight.insight', ({ strapi }) => ({
+    async create(ctx) {
+        const { answer } = ctx.request.body
+
+        const createInsight = await strapi.db
+            .query('api::insight.insight')
+            .create({
+                data: {
+                    user: ctx.state.user.id,
+                    answer: answer,
+                }
+            })
+
+        return createInsight
+    },
+
+    async find(ctx) {
+        const insight = await strapi.db
+            .query('api::insight.insight')
+            .findOne({
+                where: {
+                    user: ctx.state.user.id
+                }
+            })
+
+        if (!insight) {
+            return {
+                answer: null
+            }
+        }
+
+        return insight
+    }
+}));
